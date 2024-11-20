a430891f2a5f035f100e968b3a932fa697e7fc52
Abdusalom <abdusalom@gmail.com>
2024-11-20 13:08:26 +0500

    Made AppBar Part of the Application.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/customs/custom_size.dart';
+import 'package:pomodoro/customs/custom_text.dart';
 import 'package:pomodoro/utils/colors.dart';
+import 'package:pomodoro/utils/img_paths.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,6 +11,146 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: UtilsColors.bg,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            // App Bar Simulator
+            ClipRect(
+              child: Container(
+                height: CustomSize.height(context, 6.5),
+                color: UtilsColors.pink,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          child: Image(
+                            image: AssetImage(ImgPaths.appBarCircleLeft),
+                            width: CustomSize.width(context, 4),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        // Big Black Tomato Center
+                        Transform.translate(
+                          offset: Offset(CustomSize.width(context, 31),
+                              -CustomSize.width(context, 90)),
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: Image(
+                              image: AssetImage(ImgPaths.appBarTomatoBlack),
+                              width: CustomSize.width(context, 7.7),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        // Small Black Tomato
+                        Transform.translate(
+                          offset: Offset(CustomSize.width(context, 5.1),
+                              -CustomSize.width(context, 63)),
+                          child: Container(
+                            alignment: Alignment.bottomLeft,
+                            child: Image(
+                              image:
+                                  AssetImage(ImgPaths.appBarTomatoBlackSmall),
+                              width: CustomSize.width(context, 14.3),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        // Big Black Tomato Corner
+                        Transform.translate(
+                          offset: Offset(-CustomSize.width(context, 30),
+                              CustomSize.width(context, 37)),
+                          child: Container(
+                            alignment: Alignment.bottomLeft,
+                            child: Image(
+                              image: AssetImage(ImgPaths.appBarTomatoBlack),
+                              width: CustomSize.width(context, 8),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Text Part
+                    Transform.translate(
+                      offset: Offset(-CustomSize.width(context, 39), 0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CustomText(
+                              "Pomodoro",
+                              UtilsColors.bg,
+                              CustomSize.width(context, 16.3),
+                              FontWeight.w500,
+                              TextAlign.center,
+                              "PatuaOne"),
+                        ],
+                      ),
+                    ),
+                    Stack(
+                      children: [
+                        Container(
+                          alignment: Alignment.bottomRight,
+                          child: Image(
+                            image: AssetImage(ImgPaths.appBarCircleRight),
+                            width: CustomSize.width(context, 5),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        // Small Black Tomato
+                        Transform.translate(
+                          offset: Offset(-CustomSize.width(context, 70),
+                              -CustomSize.width(context, 35)),
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: Image(
+                              image:
+                                  AssetImage(ImgPaths.appBarTomatoBlackSmall),
+                              width: CustomSize.width(context, 14.3),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        // Big Black Tomato Center
+                        Transform.translate(
+                          offset: Offset(CustomSize.width(context, 15),
+                              -CustomSize.width(context, 17)),
+                          child: Container(
+                            alignment: Alignment.bottomRight,
+                            child: Image(
+                              image: AssetImage(ImgPaths.appBarTomatoBlack),
+                              width: CustomSize.width(context, 7.7),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        // Big Black Tomato Left
+                        Transform.translate(
+                          offset: Offset(-CustomSize.width(context, 13.9),
+                              CustomSize.width(context, 37)),
+                          child: Container(
+                            alignment: Alignment.bottomCenter,
+                            child: Image(
+                              image: AssetImage(ImgPaths.appBarTomatoBlack),
+                              width: CustomSize.width(context, 8),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/utils/img_paths.dart
+++ lib/utils/img_paths.dart
@@ -2,6 +2,7 @@ class ImgPaths {
   static String appBarCircleLeft = "assets/images/app_bar_circle_left.png";
   static String appBarCircleRight = "assets/images/app_bar_circle_right.png";
   static String appBarTomatoBlack = "assets/images/app_bar_tomato.png";
+  static String appBarTomatoBlackSmall = "assets/images/app_bar_tomato_small.png";
   static String bigTomato = "assets/images/big_tomato.png";
   static String splashTomato = "assets/images/splash_tomato.png";
   static String splashTomato2 = "assets/images/splash_tomato2.png";
