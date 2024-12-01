23c9c81328f83eab3d71b21f640d8e5df475e0b8
Abdusalom <abdusalom@gmail.com>
2024-12-01 12:19:32 +0500

    Added logic to the Task Finishing Part and now it will automaticly remove completed tasks from the available tasks and add that task to the completed tasks.

--- lib/providers/to_do_tasks.dart
+++ lib/providers/to_do_tasks.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/moduls/unfinshed_tasks_md.dart';
+import 'package:pomodoro/providers/completed_tasks.dart';
+import 'package:provider/provider.dart';
 
 class ToDoTasks extends ChangeNotifier {
   List<AllTasksModul> tasks = [
@@ -13,7 +15,8 @@ class ToDoTasks extends ChangeNotifier {
         "${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}"),
   ];
 
-  void removeTask(int index) {
+  void removeTask(int index, BuildContext context) {
+    Provider.of<CompletedTasks>(context, listen: false).addNewCompletedTask(tasks[index]);
     tasks.removeAt(index);
     notifyListeners();
   }
